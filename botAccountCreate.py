8b7fc171f52d88a62505abdbf21bd069e48082af
priyaank2004 <79186204+priyaank2004@users.noreply.github.com>
2021-04-05 11:09:29 +0530

    Add files via upload

---
+++ botAccountCreate.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import accountInfoGenerator as account
+import getVerifCode as verifiCode
+import fakeMail as email
+import random
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent={userAgent}')
+driver = webdriver.Chrome(options=options, executable_path=r"C:\Program Files\Google\Chrome\Application\chromedriver.exe")
+driver.get("https://www.instagram.com/accounts/emailsignup/")
+time.sleep(8)
+name = account.username()
+fame = account.getPassword()
+
+#Fill the email value
+email_field = driver.find_element_by_name('emailOrPhone')
+fake_email = email.getFakeMail()
+email_field.send_keys(fake_email)
+print(fake_email)
+
+# Fill the fullname value
+fullname_field = driver.find_element_by_name('fullName')
+fullname_field.send_keys(account.generatingName())
+print(account.generatingName())
+# Fill username value
+username_field = driver.find_element_by_name('username')
+username_field.send_keys(name)
+print(name)
+# Fill password value
+password_field = driver.find_element_by_name('password')
+password_field.send_keys(fame)  # You can determine another password here.
+print(fame)
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/form/div[7]/div/button"))).click()
+
+time.sleep(8)
+
+#Birthday verification
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[1]/select/option[4]"))).click()
+
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[2]/select/option[10]"))).click()
+
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select").click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[4]/div/div/span/span[3]/select/option[27]"))).click()
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='react-root']/section/main/div/div/div[1]/div/div[6]/button"))).click()
+time.sleep(3)
+#
+fMail = fake_email[0].split("@")
+mailName = fMail[0]
+domain = fMail[1]
+instCode = verifiCode.getInstVeriCode(mailName, domain, driver)
+driver.find_element_by_name('email_confirmation_code').send_keys(instCode, Keys.ENTER)
---
+++ proxy_service.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+import time
+
+# change 'ip:port' with your proxy's ip and port
+proxy_ip_port = '103.4.94.11:3128'
+
+proxy = Proxy()
+proxy.proxy_type = ProxyType.MANUAL
+proxy.http_proxy = proxy_ip_port
+proxy.ssl_proxy = proxy_ip_port
+
+capabilities = webdriver.DesiredCapabilities.CHROME
+proxy.add_to_capabilities(capabilities)
+
+# replace 'your_absolute_path' with your chrome binary absolute path
+driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe", desired_capabilities=capabilities)
+
+driver.get('http://whatismyipaddress.com')
+
+time.sleep(18)
+
+driver.quit()
